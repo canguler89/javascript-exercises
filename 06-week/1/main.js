742452783cabeb84b7ce54889f5ca7214d31e15f
Can Guler <canmuhammetguler@gmail.com>
2020-08-12 21:21:24 -0500

    variables exercise added

---
+++ 06-week/1/main.js
@@ -0,0 +1,14 @@
+// 0/0 is NaN
+// 1 + NaN
+// 1 / 0
+// infinity
+
+// ..............
+// Variables
+
+let name = "Can";
+let lastName = "Guler";
+
+let fullName = name + " " + lastName;
+
+document.getElementById("container").innerHTML = fullName;
