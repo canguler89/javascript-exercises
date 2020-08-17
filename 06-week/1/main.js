14002ede8c3b7fdb0f00758c45bca3aaeecf3ba1
Can Guler <canmuhammetguler@gmail.com>
2020-08-16 21:24:32 -0500

    algroritm challanges

--- 06-week/1/main.js
+++ 06-week/1/main.js
@@ -3,7 +3,7 @@
 // 1 / 0
 // infinity
 
-// ..............
+// ...............
 // Variables
 
 let name = "Can";
@@ -11,4 +11,34 @@ let lastName = "Guler";
 
 let fullName = name + " " + lastName;
 
-document.getElementById("container").innerHTML = fullName;
+document.getElementById("container").innerText = fullName;
+
+// ...............
+
+var x = 2;
+
+{
+  var x = 5;
+  console.log(x);
+}
+console.log(x);
+
+// output 5 x2;
+
+// ...............
+
+let y = 3;
+
+{
+  let y = 8;
+  console.log(y);
+}
+console.log(y);
+// output first 8 in a scope; then 3 out of scope..
+
+const z = 5;
+{
+  const z = 11;
+  console.log(z);
+}
+console.log(z);
