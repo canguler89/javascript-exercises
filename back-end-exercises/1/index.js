573b00dc97a863f60a58cff90c118f1b0c050088
Can Guler <canmuhammetguler@gmail.com>
2020-09-11 00:59:15 -0500

    listing JSON objects on the node route

--- back-end-exercises/1/index.js
+++ back-end-exercises/1/index.js
@@ -2,14 +2,32 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello Express!");
-});
+// app.get("/movies", (req, res) => {
+//   console.log(req.query.sort);
+// output sort=asc or sort=des
+
+//   res.send("[Movies]");
+// });
 // dynamic routes
-app.get("/movies/:genre/:year", function (req, res) {
-  console.log(req.params.genre);
-  console.log(req.params.year);
-  res.send("Movies route with year and genre");
+// app.get("/movies/:genre/:year", function (req, res) {
+//   console.log(req.params.genre);
+//   console.log(req.params.year);
+//   res.send("Movies route with year and genre");
+// });
+
+// /////////////////////
+// sending json object
+// app.get("/movies", (req, res) => {
+//   let movie = { title: "Lord of the Rings", year: 2011 };
+//   res.json(movie);
+// });
+app.get("/movies", (req, res) => {
+  let movies = [
+    { title: "Lord of the rings", year: 2011 },
+    { title: "Spider Man", year: 2018 },
+    { title: "The Girl next door", year: 2009 },
+  ];
+  res.json(movies);
 });
 
 app.listen(PORT, () => {
