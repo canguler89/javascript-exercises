e5260a4bd1d0b0d84755eb3b2a4b9907a475db75
Can Guler <canmuhammetguler@gmail.com>
2020-09-13 13:06:42 -0500

    post request

--- back-end-exercises/1/index.js
+++ back-end-exercises/1/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const { json } = require("body-parser");
 const PORT = 3000;
 
+app.use(bodyParser.json());
 // app.get("/movies", (req, res) => {
 //   console.log(req.query.sort);
 // output sort=asc or sort=des
@@ -21,13 +24,33 @@ const PORT = 3000;
 //   let movie = { title: "Lord of the Rings", year: 2011 };
 //   res.json(movie);
 // });
+
+app.get("/", (req, res) => {
+  res.send("Homepage");
+});
+
+// app.get("/movies", (req, res) => {
+//   let movies = [
+//     { title: "Lord of the rings", year: 2011 },
+//     { title: "Spider Man", year: 2018 },
+//     { title: "The Girl next door", year: 2009 },
+//   ];
+//   res.json(movies);
+// });
+// //////////////////////
+// POST request
 app.get("/movies", (req, res) => {
-  let movies = [
-    { title: "Lord of the rings", year: 2011 },
-    { title: "Spider Man", year: 2018 },
-    { title: "The Girl next door", year: 2009 },
-  ];
-  res.json(movies);
+  res.send("Movies");
+});
+
+app.post("/movies", (req, res) => {
+  let title = req.body.title;
+  let year = req.body.year;
+  let revenue = req.body.revenue;
+  console.log(title);
+  console.log(year);
+  console.log(revenue);
+  res.send("OK");
 });
 
 app.listen(PORT, () => {
