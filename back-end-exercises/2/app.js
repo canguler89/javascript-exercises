51075a50407001bffb03baf421f56440cb533f55
Can Guler <canmuhammetguler@gmail.com>
2020-09-19 21:30:05 -0500

    adding middleware example to the app

--- back-end-exercises/2/app.js
+++ back-end-exercises/2/app.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const app = express();
 
+app.use(log);
+
+function log(req, res, next) {
+  console.log("Logged");
+  console.log("Logging code 1");
+  console.log("Logging code 2");
+
+  next();
+}
+
 app.get("/", (req, res) => {
   res.send("ROOT");
 });
+app.get("/login", (req, res) => {
+  res.send("LOGGGINNN!!!!");
+});
 
 app.listen(3000, () => {
-  console.log("Server is up and running!!!1");
+  console.log("Server is up and running!!!");
 });
