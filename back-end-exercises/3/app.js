413f09b319ddd7be6cfdaab4734f845b49805776
Can Guler <canmuhammetguler@gmail.com>
2020-09-21 22:52:23 -0500

    first row insert to the database

--- back-end-exercises/3/app.js
+++ back-end-exercises/3/app.js
@@ -2,4 +2,13 @@ const pgp = require("pg-promise")();
 const connectionString = "postgres://localhost:5432/garden";
 const db = pgp(connectionString);
 
-console.log(db);
+db.none("INSERT INTO dishes(name,course,price,imageURL) VALUES($1,$2,$3,$4)", [
+  "Chicked Salad",
+  "Entrees",
+  7.5,
+  "chickedsalad.png",
+])
+  .then(() => {
+    console.log("SUCCESS");
+  })
+  .catch((error) => console.log(error));
