0f082beab32c2b300f699d257a818e9482dd7f36
Can Guler <canmuhammetguler@gmail.com>
2020-09-22 20:16:32 -0500

    get the data with .any method

--- back-end-exercises/3/app.js
+++ back-end-exercises/3/app.js
@@ -2,13 +2,29 @@ const pgp = require("pg-promise")();
 const connectionString = "postgres://localhost:5432/garden";
 const db = pgp(connectionString);
 
-db.none("INSERT INTO dishes(name,course,price,imageURL) VALUES($1,$2,$3,$4)", [
-  "Chicked Salad",
-  "Entrees",
-  7.5,
-  "chickedsalad.png",
-])
-  .then(() => {
-    console.log("SUCCESS");
+// get the items from database
+db.any("SELECT name,course,price,imageURL FROM dishes WHERE price > 5;")
+  .then((dishes) => {
+    console.log(dishes);
   })
-  .catch((error) => console.log(error));
+  .catch((error) => console.error(error));
+
+// db.none("INSERT INTO dishes(name,course,price,imageURL) VALUES($1,$2,$3,$4)", [
+//   "Chicked Salad",
+//   "Entrees",
+//   7.5,
+//   "chickedsalad.png",
+// ])
+//   .then(() => {
+//     console.log("SUCCESS");
+//   })
+//   .catch((error) => console.log(error));
+
+// db.one(
+//   "INSERT INTO dishes(name,course,price,imageURL) VALUES($1,$2,$3,$4) RETURNING dishid",
+//   ["Soup", "Starters", 5.5, "soup.png"]
+// )
+//   .then((data) => {
+//     console.log(data.dishid);
+//   })
+//   .catch((error) => console.log(error));
