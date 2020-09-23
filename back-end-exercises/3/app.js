4da93a008c12ecb88dbc847e068f0a20f5252257
Can Guler <canmuhammetguler@gmail.com>
2020-09-22 20:23:55 -0500

    using pg promise updated the database

--- back-end-exercises/3/app.js
+++ back-end-exercises/3/app.js
@@ -2,12 +2,18 @@ const pgp = require("pg-promise")();
 const connectionString = "postgres://localhost:5432/garden";
 const db = pgp(connectionString);
 
-// get the items from database
-db.any("SELECT name,course,price,imageURL FROM dishes WHERE price > 5;")
-  .then((dishes) => {
-    console.log(dishes);
+db.none("UPDATE dishes SET price = $1 WHERE dishid = $2", [6.5, 5])
+  .then(() => {
+    console.log("UPDATED");
   })
-  .catch((error) => console.error(error));
+  .catch((error) => console.log(error));
+
+// get the items from database
+// db.any("SELECT name,course,price,imageURL FROM dishes WHERE price > $1;", [5])
+//   .then((dishes) => {
+//     console.log(dishes);
+//   })
+//   .catch((error) => console.error(error));
 
 // db.none("INSERT INTO dishes(name,course,price,imageURL) VALUES($1,$2,$3,$4)", [
 //   "Chicked Salad",
