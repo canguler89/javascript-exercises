c7cc40354b7699acf86e85f2570ea8c6f0ecbf0a
Can Guler <canmuhammetguler@gmail.com>
2020-09-24 00:05:50 -0500

    send first data to sql db

--- newsapp/app.js
+++ newsapp/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const mustacheExpress = require("mustache-express");
 const app = express();
+const mustacheExpress = require("mustache-express");
 const bodyParser = require("body-parser");
+const pgp = require("pg-promise")();
 const PORT = 3000;
+const CONNECTION_STRING = "postgres://localhost:5432/newsdb";
 
 // engine
 app.engine("mustache", mustacheExpress());
@@ -11,12 +13,33 @@ app.set("view engine", "mustache");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+const db = pgp(CONNECTION_STRING);
+
 app.post("/register", (req, res) => {
   let username = req.body.username;
   let password = req.body.password;
+
+  db.oneOrNone("SELECT userid FROM users WHERE username = $1", [username]).then(
+    (user) => {
+      if (user) {
+        res.render("register", { message: "USERNAME already exists!" });
+      } else {
+        db.none("INSERT INTO users(username,password) VALUES($1,$2)", [
+          username,
+          password,
+        ])
+          .then(() => {
+            res.send("success");
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    }
+  );
+
   console.log(username);
   console.log(password);
-  res.send("success");
 });
 
 app.get("/register", (req, res) => {
