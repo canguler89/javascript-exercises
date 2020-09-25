32adb407fd566de88654ce3fac7b71e0d6ab290f
Can Guler <canmuhammetguler@gmail.com>
2020-09-25 12:10:38 -0500

    created partials for mustache pages

--- newsapp/app.js
+++ newsapp/app.js
@@ -4,18 +4,65 @@ const mustacheExpress = require("mustache-express");
 const bodyParser = require("body-parser");
 const pgp = require("pg-promise")();
 const bcrypt = require("bcrypt");
+const session = require("express-session");
+const path = require("path");
+
 const PORT = 3000;
 const CONNECTION_STRING = "postgres://localhost:5432/newsdb";
 const SALT_ROUNDS = 10;
 
+const VIEWS_PATH = path.join(__dirname, "./views");
 // engine
-app.engine("mustache", mustacheExpress());
-app.set("views", "./views");
+app.engine("mustache", mustacheExpress(VIEWS_PATH + "/partials", ".mustache"));
+app.set("views", VIEWS_PATH);
 app.set("view engine", "mustache");
+
+app.use(
+  session({
+    secret: "adasdasd",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const db = pgp(CONNECTION_STRING);
+
+app.get("/users/articles", (req, res) => {
+  res.render("articles", { username: req.session.user.username });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+
+  db.oneOrNone(
+    "SELECT userid,username,password FROM  users WHERE username = $1",
+    [username]
+  ).then((user) => {
+    if (user) {
+      bcrypt.compare(password, user.password, function (error, result) {
+        if (result) {
+          // username and userId in the session
+          if (req.session) {
+            req.session.user = { userId: user.userid, username: user.username };
+          }
+
+          res.redirect("/users/articles");
+        } else {
+          res.render("login", { message: "Invalid username or password!" });
+        }
+      });
+    } else {
+      res.render("login", { message: "Invalid username or password!" });
+    }
+  });
+});
 
 app.post("/register", (req, res) => {
   let username = req.body.username;
