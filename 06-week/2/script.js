5f1b748f8fc93d3b5bbbf5f55c5cd78f300b8c80
Can Guler <canmuhammetguler@gmail.com>
2020-10-28 21:23:44 -0500

    while guess loop

--- 06-week/2/script.js
+++ 06-week/2/script.js
@@ -12,9 +12,35 @@ console.log(reversed)
 // /////////////////////
 // Nested for loops
 
-for(let i = 0; i <= 10; i++) {
-    console.log(i)
-    for(let j = 10; j >= 4; j-=2){
-        console.log(j)
-    }
+// for(let i = 0; i <= 10; i++) {
+//     console.log(i)
+//     for(let j = 10; j >= 4; j-=2){
+//         console.log(j)
+//     }
+// }
+// another example
+// const gameboard = [
+//     [4,32,8,4],
+//     [64,8,32,2],
+//     [8,32,16,4],
+//     [2,8,4,2]
+// ]
+//     let total = 0
+// for(let i = 0; i < gameboard.length; i++){
+//     let row = gameboard[i]
+//     for(let j = 0; j < row.length; j++){
+//         total += row[j]
+//     }
+// }
+// console.log(total)
+
+const target = Math.floor(Math.random() * 10)
+let guess = Math.floor(Math.random() * 10)
+
+
+while(guess !== target){
+    console.log("target:", target, "guess: ", guess)
+    guess = Math.floor(Math.random() * 10)
 }
+
+console.log(`CONGRATZ target: ${target} guess: ${guess}`)
