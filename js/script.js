295491187a41379a7ea5bc9141f0493a5eaa5853
Can Guler <canmuhammetguler@gmail.com>
2020-11-28 03:15:56 -0600

    some exercises

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// function findMissings(arr) { 
+
+//     count = arr[arr.length - 1];
+//     let misNumber = [];
+//     for( let i = arr[0]; i <= count; i++){
+//       if(arr.indexOf(i) == -1){
+//         misNumber.push(i)
+//       }
+//     }
+//     let total = misNumber.length;
+    
+//     document.getElementById("arr1").innerHTML = arr
+//     document.getElementById("func").innerHTML = misNumber;
+//     document.getElementById("total").innerHTML = total
+//     return total; 
+  
+//   }
+  
+//   console.log(findMissings([1,5]));
+let newArr = []
+let myArr = [1,2,3,4,5,6]
+
+// newArr.push(myArr[myArr.length -1])
+// console.log(myArr.slice(-1))
+// console.log(newArr)
+
+
+function findNumbs(arr){
+
+  let missNumbers = [];
+  for(let i= arr[0];i <= arr.slice(-1); i++){
+    if(arr.indexOf(i) == -1){
+      missNumbers.push(i)
+    }
+  }
+  console.log(missNumbers)
+}
+
+findNumbs([1,7])
